7bccbef739cf4d76162278636a8eb37307aec2b9
=LC Thanh <lcthanh.htvn@gmail.com>
2023-03-21 14:43:22 +0700

    "learn dictionaries"

--- main.py
+++ main.py
@@ -6,9 +6,22 @@ OUT = open("./OUTPUT_DATA.txt", "w")
 # for str in strings:
 #     OUT.writelines([str, "\n"])
 
-dict1 = dict()
-dict1['a'] = 2003
-print('b' in dict1)
+# dict1 = dict()
+# dict1['a'] = 2003
+# print('b' in dict1)
+# print(dict1.get('a', 0))
+
+
+words = dict()
+line = input('Enter a line of text: ')
+words = line.split()
+count = dict()
+
+print('Counting...')
+for word in words:
+    count[word] = count.get(word, 0) + 1
+
+print('Count: ', count)
 
 INP.close()
 OUT.close()
