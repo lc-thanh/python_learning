a3de940046e84814e65b9f6c68e0c17a7f9b3445
=LC Thanh <lcthanh.htvn@gmail.com>
2023-03-21 19:09:20 +0700

    wordMaxCount() method

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-INP = open("./DATA.txt")
-OUT = open("./OUTPUT_DATA.txt", "w")
+# INP = open("./DATA.txt")
+# OUT = open("./OUTPUT_DATA.txt", "w")
 
 # string = INP.read()
 # strings = string.split(' ')
@@ -12,23 +12,59 @@ OUT = open("./OUTPUT_DATA.txt", "w")
 # print(dict1.get('a', 0))
 
 
-words = dict()
-line = input('Enter a line of text: ')
-words = line.split()
-count = dict()
+# words = dict()
+# line = input('Enter a line of text: ')
+# words = line.rstrip().split()
+# count = dict()
+#
+# print('Counting...')
+# for word in words:
+#     count[word] = count.get(word, 0) + 1
+#
+# print('Count: ', count)
 
-print('Counting...')
-for word in words:
-    count[word] = count.get(word, 0) + 1
 
-print('Count: ', count)
+def maxWordCount(input_file: str, output_file: str):
+    """
+    Hàm đếm số lượng xuất hiện của mỗi từ trong file 'input_file' và
+    đưa ra từ (hoặc các từ) có số lượng xuất hiện nhiều nhất vào file 'output_file'
+    theo format "<Từ> <Số lượng>"
+    :param input_file: File đầu vào
+    :param output_file: File đầu ra
+    :return: True, nếu thực thi thành công | False, nếu không thành công
+    """
 
-INP.close()
-OUT.close()
+    INP = open(input_file)
+    OUT = open(output_file, "w")
+
+    # Đọc từng dòng dữ liệu trong file INP và tách ra từng chữ (words)
+    # và đếm số lần xuất hiện của từng chữ đó
+    counts = dict()
+    for line in INP:
+        words = line.split()
+        for word in words:
+            counts[word] = counts.get(word, 0) + 1
+
+    # Tìm số lần xuất hiện lớn nhất
+    maxCount = None
+    for count in counts.values():
+        if maxCount is None or count > maxCount:
+            maxCount = count
+
+    # Ghi kết quả vào file OUT theo format: "word maxCount"
+    for word, count in counts.items():
+        if count == maxCount:
+            OUT.write(str(word))
+            OUT.write(' ')
+            OUT.write(str(count))
+            OUT.write('\n')
+
+    INP.close()
+    OUT.close()
 
 
 def Test():
-    pass
+    maxWordCount("DATA.txt", "OUTPUT_DATA.txt")
 
 
 if __name__ == "__main__":
